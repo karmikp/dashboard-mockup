6704dbc193688127bb5e431e0249e1b981f2497f
Andrey Danil <adanilko@hawk.iit.edu>
2016-06-29 11:44:53 -0500

    adding demo data 🗄

--- assets/demo/demo-cards.js
+++ assets/demo/demo-cards.js
@@ -1,11 +1,48 @@
 $(function() {
     var data = {
+        series: [1, 1]
+    };
+
+    var data2 = {
+        series: [1, 2]
+    };
+
+    var data3 = {
         series: [1, 5]
+    };
+
+    var data4 = {
+        series: [1, 3, 5, 2]
     };
 
     var sum = function(a, b) { return a + b };
 
+
     new Chartist.Pie('#chartistPie', data, {
+        labelInterpolationFnc: function(value) {
+            return '';
+        }
+    });
+
+    new Chartist.Pie('#chartistPie1', data, {
+        labelInterpolationFnc: function(value) {
+            return '';
+        }
+    });
+
+    new Chartist.Pie('#chartistPie2', data2, {
+        labelInterpolationFnc: function(value) {
+            return '';
+        }
+    });
+
+    new Chartist.Pie('#chartistPie3', data3, {
+        labelInterpolationFnc: function(value) {
+            return '';
+        }
+    });
+
+    new Chartist.Pie('#chartistPie4', data4, {
         labelInterpolationFnc: function(value) {
             return '';
         }
